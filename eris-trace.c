a2d9e3a59246b421d5e1c76b136c47f4a986ac2d
Adrian Perez de Castro <aperez@igalia.com>
2015-04-24 20:44:22 +0200

    Implement a simple run-time tracing mechanism
    Building with the ERIS_TRACE=1 preprocessor symbol defined enables
    building the tracing system. Even when built, it is disabled by
    default, and the ERIS_TRACE environment variable must be defined
    (and not empty) to enable the trace. Overhead is minimal, and no
    function calls to eris_trace() are done if tracing is built but
    disabled. If tracing support is not compiled in, no additional
    code is ever generated by the compiler.

---
+++ eris-trace.c
@@ -0,0 +1,134 @@
+/*
+ * eris-util.c
+ * Copyright (C) 2015 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "eris-trace.h"
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#undef eris_trace_setup
+
+
+enum {
+    TRACE_NONE = 0,
+    TRACE_FILE = (1 << 1),
+    TRACE_LINE = (1 << 2),
+    TRACE_FUNC = (1 << 3),
+    TRACE_ALL  = (TRACE_FILE | TRACE_LINE | TRACE_FUNC)
+};
+
+
+/*
+ * These values are configured by eris_trace_setup().
+ */
+bool eris_trace_enabled     = false;
+static FILE* trace_output   = NULL;
+static unsigned trace_items = TRACE_NONE;
+
+
+void
+eris_trace (const char *file,
+            unsigned    line,
+            const char* func,
+            const char* fmt,
+            ...)
+{
+    bool insert_space = false;
+    if (trace_items & TRACE_FILE) {
+        fprintf (trace_output, "%s:", file);
+        insert_space = true;
+    }
+    if (trace_items & TRACE_FUNC) {
+        fprintf (trace_output, "%s:", func);
+        insert_space = true;
+    }
+    if (trace_items & TRACE_LINE) {
+        fprintf (trace_output, "%u:", line);
+        insert_space = true;
+    }
+
+    if (insert_space) {
+        fputc (' ', trace_output);
+    }
+
+    va_list args;
+    va_start (args, fmt);
+    vfprintf (trace_output, fmt, args);
+    va_end (args);
+
+    fflush (trace_output);
+}
+
+
+void
+eris_trace_setup (void)
+{
+    /* Tracing was already configured. */
+    if (trace_output != NULL)
+        return;
+
+    trace_output = stderr;
+    const char *env_value = getenv ("ERIS_TRACE");
+
+    /* Check for an empty environment variable. */
+    if (!env_value || !*env_value)
+        return;
+
+    bool file_append = false;
+
+    /* Check flags. */
+    for (; *env_value; env_value++) {
+        switch (*env_value) {
+            case 'L':
+                trace_items |= TRACE_LINE;
+                break;
+            case 'S':
+                trace_items |= TRACE_FILE;
+                break;
+            case 'F':
+                trace_items |= TRACE_FUNC;
+                break;
+            case 'A':
+                trace_items |= TRACE_ALL;
+                break;
+
+            case 'l':
+                trace_items &= ~TRACE_LINE;
+                break;
+            case 's':
+                trace_items &= ~TRACE_FILE;
+                break;
+            case 'f':
+                trace_items &= ~TRACE_FUNC;
+                break;
+
+            case '>':
+                file_append = true;
+                /* fall-through */
+            case ':':
+                env_value++;
+                trace_output = fopen (env_value,
+                                      file_append ? "ab" : "wb");
+                if (!trace_output) {
+                    fprintf (stderr,
+                             "Could not open '%s' for %s (%s), using stderr\n",
+                             env_value, file_append ? "appending" : "writing",
+                             strerror (errno));
+                    fflush (stderr);
+                    trace_output = stderr;
+                }
+                goto setup_done;
+                break;
+        }
+    }
+
+setup_done:
+    eris_trace_enabled = true;
+}
---
+++ eris-trace.h
@@ -0,0 +1,53 @@
+/*
+ * eris-trace.h
+ * Copyright (C) 2015 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef ERIS_TRACE_H
+#define ERIS_TRACE_H
+
+#include <stdbool.h>
+
+#if defined(ERIS_TRACE) && ERIS_TRACE > 0
+# define TRACE(...)                                                 \
+    do {                                                            \
+        if (eris_trace_enabled)                                     \
+            eris_trace (__FILE__, __LINE__, __func__, __VA_ARGS__); \
+    } while (0)
+
+extern void eris_trace (const char* file,
+                        unsigned    line,
+                        const char* func,
+                        const char* fmt,
+                        ...);
+
+/*
+ * Reads the ERIS_TRACE environment variable and, if present, configures the
+ * tracing mechanism according to its value. The value of the variable must
+ * be a sequence of characters, which enable printing of different items
+ * along with each message passed to the TRACE() macro:
+ *
+ *   'S' - source file name.
+ *   'L' - line in source file.
+ *   'F' - function name.
+ *   'A' - all of the above.
+ *
+ * The lowercase counterparts disable printing of the corresponding item.
+ * Characters other than the above (either upper- or lowercase) are ignored.
+ *
+ * Also, if the '>' or ':' characters are found, the rest of the value of the
+ * ERIS_TRACE environment variable is taken as the name of a file to open for
+ * writing (when using ':'), or appending messages (when using '>').
+ */
+extern void eris_trace_setup (void);
+
+extern bool eris_trace_enabled;
+
+#else
+# define eris_trace_setup( )  ((void)0)
+# define TRACE(...)           ((void)0)
+#endif /* ERIS_TRACE */
+
+#endif /* !ERIS_TRACE_H */
