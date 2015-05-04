cbe11df4470f38cb71c0134a3988faa41f6cd8ad
Adrian Perez de Castro <aperez@igalia.com>
2015-05-04 14:02:31 +0200

    testutil: Add realpath(), isfile(), and isdir()
    Adds the following functions:
    
    - realpath() canonicalizes a path to an existing file.
    - isfile() checks whether a path refers to an existing, regular file.
    - isdir() checks whether a path refers to an existing directory.

--- testutil-module.c
+++ testutil-module.c
@@ -9,10 +9,14 @@
 #include "lua/src/lauxlib.h"
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+#include <limits.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 
 
@@ -84,11 +88,59 @@ testutil_listdir (lua_State *L)
 }
 
 
+static int
+testutil_realpath (lua_State *L)
+{
+    const char *path = luaL_checkstring (L, 1);
+    char buffer[PATH_MAX];
+    if (realpath (path, buffer) == NULL) {
+        return luaL_error (L, "%s: %s", path, strerror (errno));
+    }
+    lua_pushstring (L, buffer);
+    return 1;
+}
+
+
+static int
+testutil_isfile (lua_State *L)
+{
+    struct stat sb;
+    const char *path = luaL_checkstring (L, 1);
+    if (stat (path, &sb) == 0) {
+        lua_pushboolean (L, S_ISREG (sb.st_mode));
+    } else if (errno == ENOENT || errno == ENOTDIR) {
+        lua_pushboolean (L, false);
+    } else {
+        return luaL_error (L, "%s: %s", path, strerror (errno));
+    }
+    return 1;
+}
+
+
+static int
+testutil_isdir (lua_State *L)
+{
+    struct stat sb;
+    const char *path = luaL_checkstring (L, 1);
+    if (stat (path, &sb) == 0) {
+        lua_pushboolean (L, S_ISDIR (sb.st_mode));
+    } else if (errno == ENOENT || errno == ENOTDIR) {
+        lua_pushboolean (L, false);
+    } else {
+        return luaL_error (L, "%s: %s", path, strerror (errno));
+    }
+    return 1;
+}
+
+
 static const luaL_Reg testutillib[] = {
-    { "isatty",  testutil_isatty  },
-    { "fork",    testutil_fork    },
-    { "waitpid", testutil_waitpid },
-    { "listdir", testutil_listdir },
+    { "isatty",   testutil_isatty   },
+    { "fork",     testutil_fork     },
+    { "waitpid",  testutil_waitpid  },
+    { "listdir",  testutil_listdir  },
+    { "realpath", testutil_realpath },
+    { "isfile",   testutil_isfile   },
+    { "isdir",    testutil_isdir    },
     { NULL, NULL }
 };
 
