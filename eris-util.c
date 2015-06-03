3a0947cafefafcdb88e776f4825fbda519071033
Adrian Perez de Castro <aperez@igalia.com>
2015-06-03 23:56:06 +0200

    With glibc, print partial backtrace on failed runtime checks
    This uses the backtrace() and backtrace_symbols() functions present in
    glibc to present a partial backtrace up to the point where a check failed.

--- eris-util.c
+++ eris-util.c
@@ -10,6 +10,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#if defined(__GLIBC__) && __GLIBC__ >= 2
+# ifndef ERIS_CHECK_BACKTRACE
+# define ERIS_CHECK_BACKTRACE 5
+# endif /* !ERIS_CHECK_BACKTRACE */
+# include <execinfo.h>
+#else
+# undef ERIS_CHECK_BACKTRACE
+#endif /* __GLIBC__ */
+
 
 void
 eris_runtime_check_failed (const char *file,
@@ -18,8 +27,20 @@ eris_runtime_check_failed (const char *file,
                            const char *fmt,
                            ...)
 {
+#ifdef ERIS_CHECK_BACKTRACE
+    void *addresses[ERIS_CHECK_BACKTRACE + 1];
+    size_t size = backtrace (addresses, LENGTH_OF (addresses));
+    char **names = backtrace_symbols (addresses, size);
+
+    fprintf (stderr, "\nSTACK:[37m\n");
+    for (size_t i = size; i > 1; i--)
+        fprintf (stderr, "%2u | %s\n", (unsigned) (i - 2), names[i-1]);
+    free (names);
+#endif /* ERIS_CHECK_BACKTRACE */
+
     fprintf (stderr,
-             "=== CHECK FAILED at %s(), %s:%u ===\n",
+             "\n[1;31m=== CHECK FAILED ===[0;0m at "
+             "[1;1m%s[0;0m(), [36m%s[0m:%u\n",
              func, file, line);
 
     va_list args;
